98d73abe7e7ffb1a9ae8da020f9d6138171eeaac
Sara Blom <sara.cecilia.blom@gmail.com>
2022-12-05 20:11:31 +0100

    add data

---
+++ src/data.ts
@@ -0,0 +1,37 @@
+export const FOOD = [
+	{
+		name: "Bacon and eggs",
+		price: "4,50",
+		image: "/images_day_two/plate__bacon-eggs.png",
+	},
+	{
+		name: "Chicken Salad",
+		price: "4,50",
+		image: "/images_day_two/plate__chicken-salad.png",
+	},
+	{
+		name: "French Fries",
+		price: "4,50",
+		image: "/images_day_two/plate__french-fries.png",
+	},
+	{
+		name: "Ravioli",
+		price: "4,50",
+		image: "/images_day_two/plate__ravioli.png",
+	},
+	{
+		name: "Salmon and Vegetables",
+		price: "4,50",
+		image: "/images_day_two/plate__salmon-vegetables.png",
+	},
+	{
+		name: "Spaghetti with Meat Sauce",
+		price: "4,50",
+		image: "/images_day_two/plate__spaghetti-meat-sauce.png",
+	},
+	{
+		name: "Tortellini",
+		price: "4,50",
+		image: "/images_day_two/plate__tortellini.png",
+	},
+];
